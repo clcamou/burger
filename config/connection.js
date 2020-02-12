8541720e6e06a15f1465d3b450c8edc8757c249a
clcamou <clcamou@gmail.com>
2020-02-12 10:11:55 -0700

    added JAWSBD connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 //set up mysql
 let mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
-    host: "localhost", 
-    port: 3306, 
-    user: "root", 
-    password: "Greenapples_45", 
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL); 
+} else {
+     connection = mysql.createConnection({
+        host: "localhost", 
+        port: 3306, 
+        user: "root", 
+        password: "Greenapples_45", 
+        database: "burgers_db"
+    });
+};
 
 //make connection 
 connection.connect(function(err){
@@ -17,6 +22,9 @@ connection.connect(function(err){
     }
     console.log("connected as id " + connection.threadId);
 });
+
+
+    mysql://l0od1ugshspvafn9:akz1pj6s9k9rk8pl@wiad5ra41q8129zn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ldpkrrlc6qa837l9
 
 //export connection for our ORM to use
 module.exports = connection;
